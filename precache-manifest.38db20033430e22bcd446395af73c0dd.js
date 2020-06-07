e3e0e8b64ac66c3d3cade18599097c12c572127d
Elior Nuri <eliornuri777@gmail.com>
2020-06-07 17:00:40 +0300

    Updates

---
+++ precache-manifest.38db20033430e22bcd446395af73c0dd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8f50af6da8795cdfe06bcb7a03133c6",
+    "url": "/proj-misterBITcoin/index.html"
+  },
+  {
+    "revision": "97e1616a5527cffa0117",
+    "url": "/proj-misterBITcoin/static/css/main.a2159cef.chunk.css"
+  },
+  {
+    "revision": "1a18e724df2ffc2ccf7d",
+    "url": "/proj-misterBITcoin/static/js/2.95d6aed8.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "/proj-misterBITcoin/static/js/2.95d6aed8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e1616a5527cffa0117",
+    "url": "/proj-misterBITcoin/static/js/main.2fe6e523.chunk.js"
+  },
+  {
+    "revision": "453ac1f32cf787eb5faf",
+    "url": "/proj-misterBITcoin/static/js/runtime-main.889b61da.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/proj-misterBITcoin/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/proj-misterBITcoin/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/proj-misterBITcoin/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "9ded577f93b24331f4faadfc5f834ae6",
+    "url": "/proj-misterBITcoin/static/media/SourceSansPro-Black.9ded577f.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "/proj-misterBITcoin/static/media/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "3cf05216a746ae03e2b6e971a9c6d285",
+    "url": "/proj-misterBITcoin/static/media/logo.3cf05216.png"
+  }
+]);
