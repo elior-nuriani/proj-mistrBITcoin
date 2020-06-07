26c9629f80260e40511aed53bb708e319069647a
Elior Nuri <eliornuri777@gmail.com>
2020-06-07 17:42:12 +0300

    Updates

---
+++ precache-manifest.bffb1671bc6c05263b3cd2c07a54706f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "616fa09a5928340ea1e90447eee59873",
+    "url": "./index.html"
+  },
+  {
+    "revision": "724cee171b95ff9932e0",
+    "url": "./static/css/main.629e4e35.chunk.css"
+  },
+  {
+    "revision": "c87fb08dcf86cd238236",
+    "url": "./static/js/2.8433c078.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "./static/js/2.8433c078.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "724cee171b95ff9932e0",
+    "url": "./static/js/main.32755265.chunk.js"
+  },
+  {
+    "revision": "c72454fa52f9ce807b8f",
+    "url": "./static/js/runtime-main.62999a71.js"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "./static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "./static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "./static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "9ded577f93b24331f4faadfc5f834ae6",
+    "url": "./static/media/SourceSansPro-Black.9ded577f.ttf"
+  },
+  {
+    "revision": "c1678b46f7dd3f50ceac94ed4e0ad01a",
+    "url": "./static/media/SourceSansPro-Regular.c1678b46.ttf"
+  },
+  {
+    "revision": "3cf05216a746ae03e2b6e971a9c6d285",
+    "url": "./static/media/logo.3cf05216.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/proj-misterBITcoin/precache-manifest.209c84c8aee8d32a6eaeee1bf60963ea.js"
+  "./precache-manifest.bffb1671bc6c05263b3cd2c07a54706f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/proj-misterBITcoin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
